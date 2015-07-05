681c60d68a9ac701126fc86a776d95350b8a24de
jcane <jean.canevello@gmail.com>
2015-07-04 23:01:41 -0500

    json

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,4 +1,6 @@
 
 USE `gourmet`;
 
-ALTER TABLE  `reserva` ADD  `estado` INT NOT NULL COMMENT  '0: pendiente, 1:reservado' AFTER  `telefono` ;
+-- ALTER TABLE  `reserva` ADD  `estado` INT NOT NULL COMMENT  '0: pendiente, 1:reservado' AFTER  `telefono` ;
+
+ALTER TABLE  `reserva` CHANGE  `idreserva`  `idreserva` VARCHAR( 30 ) NOT NULL ;
